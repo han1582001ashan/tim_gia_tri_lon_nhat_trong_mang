3452e93d837c687ff7637e6c3a4ebebc9eae033d
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-19 21:33:19 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Transactions;
+namespace DisplayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int size;
+          int[] array;
+            Console.WriteLine("Enter a size: ");
+            size = Int32.Parse(Console.ReadLine());
+            array = new int[size];
+            int i=0;
+            while (i<array.Length){
+                Console.WriteLine("Enter Element "+ (i+1) + " : ");
+                array[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.WriteLine ("List: ");
+            for (int j=0; j<array.Length; j++){
+                Console.Write(array[j] + ", ");
+
+            }
+            int max = array[0];
+            int index =1;
+            for(int j=0; j<array.Length; j++){
+                if(array[j]>max){
+                    max= array[j];
+                    index= j+1;
+                }
+            }
+            Console.WriteLine("\nThe largest property value in the list is " + max + " ,at position " + index);
+      
+
+        }
+    }
+}
+
